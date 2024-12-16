eb416b6d0e1dff57cdf1e4e93dd576c783f9ed56
hamdullahozkan <hamdullahozkann@gmail.com>
2024-12-16 22:11:23 +0300

    Proje dosyası ekle.

---
+++ Patika++/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Helloworld
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hamdullah özkan");
+            Console.WriteLine("Yazılıma olan ilgim uzun yıllardır vardı kendimi bu alanda kod yazmayı hayal ediyordum sürekli, patika+ karsıma cıktı cevremden ve arastırmalarım sonucunda en iyi yolun burası oldugunu düşündüm kendımi burada geliştirip hayalim olan yazılım dunyasına hazır bir şekilde girecegime inanıyorum");
+
+
+        }
+    }
+}
